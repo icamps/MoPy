52df8ae5eb105afbcb5f05d9c9f2d7b00412dca2
Ihosvany Camps <13273938+icamps@users.noreply.github.com>
2022-08-02 14:08:03 -0300

    Update fromMOPAC.sh

--- fromMOPAC.sh
+++ fromMOPAC.sh
@@ -4,7 +4,8 @@
 for filename in $1/*.out
 do
 
-TotalE=$(grep "TOTAL ENERGY            =" $filename | grep '='| awk '{printf "%12.6f \n",$4}')
+HeatF=$(grep "FINAL HEAT OF FORMATION =      " $filename | grep '='| awk '{printf "%12.6f \n",$6}')
+TotalEnergy=$(grep "TOTAL ENERGY            =" $filename | grep '='| awk '{printf "%12.6f \n",$4}')
 DispE=$(grep "DISPERSION ENERGY       =" $filename | grep '='| awk '{printf "%12.6f \n",$4}')
 HBondE=$(grep "H-BOND ENERGY           =" $filename | grep '='| awk '{printf "%12.6f \n",$4}')
 NHBonds=$(grep "No. OF HYDROGEN BONDS   =" $filename | grep '='| awk '{printf "%12.6f \n",$6}')
@@ -15,6 +16,11 @@ BetaSOMO=$(grep "BETA  SOMO LUMO (EV)    =" $filename | grep '='| awk '{printf "
 BetaLUMO=$(grep "BETA  SOMO LUMO (EV)    =" $filename | grep '='| awk '{printf "%12.6f \n",$7}')
 HOMO=$(grep "HOMO LUMO ENERGIES (EV) =" $filename | grep '='| awk '{printf "%12.6f \n",$6}')
 LUMO=$(grep "HOMO LUMO ENERGIES (EV) =" $filename | grep '='| awk '{printf "%12.6f \n",$7}')
+
+var1=$(echo $TotalEnergy | cut -f1 -d' ')
+var2=$(echo $TotalEnergy | cut -f2 -d' ')
+TotalEkcal="$var1"
+TotalEeV="$var2"
 
 if [ "$DispE" = "" ];
 then
@@ -58,9 +64,10 @@ then
 fi
 
 
-echo $filename, $TotalE, $DispE, $HBondE, $NHBonds, $IonPot, $AlphaSOMO, $AlphaLUMO, $BetaSOMO, $BetaLUMO, $HOMO, $LUMO >> $1.t379x24
+echo $filename, $HeatF, $TotalEkcal, $TotalEeV, $DispE, $HBondE, $NHBonds, $IonPot, $AlphaSOMO, $AlphaLUMO, $BetaSOMO, $BetaLUMO, $HOMO, $LUMO >> $1.t379x24
 done
 
-echo 'Filename', 'Total Energy (eV)', 'Dispersion Energy (eV)', 'H-BOND Ebergy (eV)', 'No. HBonds', 'Ionization Potential (eV)', 'AlphaSOMO (eV)', 'AlphaLUMO (eV)', 'BetaSOMO (eV)', 'BetaLUMO (eV)', 'HOMO (eV)', 'LUMO (eV)' >> t379x24
+echo 'Filename', 'Heat of formation (kcal/mol)', 'Total Energy (kcal/mol)', 'Total Energy (eV)', 'Dispersion Energy (eV)', 'H-BOND Ebergy (eV)', 'No. HBonds', 'Ionization Potential (eV)', 'AlphaSOMO (eV)', 'AlphaLUMO (eV)', 'BetaSOMO (eV)', 'BetaLUMO (eV)', 'HOMO (eV)', 'LUMO (eV)' >> t379x24
 cat t379x24 $1.t379x24 > $1.csv
 rm -f t379x24 $1.t379x24
+
