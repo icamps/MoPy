29bb6e9d6f56c04af2cb571de78ba89ad53fa783
Ihosvany Camps <13273938+icamps@users.noreply.github.com>
2022-08-02 14:26:20 -0300

    Update fromMOPAC.sh

--- fromMOPAC.sh
+++ fromMOPAC.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 #script to extract information from the output MOPAC file
+#Use:
+# ./fromMOPAC.sh folder_name
+#
+#The folder_name is a folder where all the MOPAC output files (.out) are located. A file with the name of the folder will be created with the data.
+#
+#
 
 for filename in $1/*.out
 do
@@ -16,6 +22,8 @@ BetaSOMO=$(grep "BETA  SOMO LUMO (EV)    =" $filename | grep '='| awk '{printf "
 BetaLUMO=$(grep "BETA  SOMO LUMO (EV)    =" $filename | grep '='| awk '{printf "%12.6f \n",$7}')
 HOMO=$(grep "HOMO LUMO ENERGIES (EV) =" $filename | grep '='| awk '{printf "%12.6f \n",$6}')
 LUMO=$(grep "HOMO LUMO ENERGIES (EV) =" $filename | grep '='| awk '{printf "%12.6f \n",$7}')
+CosmoArea=$(grep "COSMO AREA              =" $filename | grep '='| awk '{printf "%12.6f \n",$4}')
+CosmoVolume=$(grep "COSMO VOLUME            =" $filename | grep '='| awk '{printf "%12.6f \n",$4}')
 
 var1=$(echo $TotalEnergy | cut -f1 -d' ')
 var2=$(echo $TotalEnergy | cut -f2 -d' ')
@@ -24,50 +32,48 @@ TotalEeV="$var2"
 
 if [ "$DispE" = "" ];
 then
-   DispE="XXXXX"
+   DispE="-----"
 fi
 if [ "$HBondE" = "" ];
 then
-   HBondE="XXXXX"
+   HBondE="-----"
 fi
 if [ "$NHBonds" = "" ];
 then
-   NHBonds="XXXXX"
+   NHBonds="-----"
 fi
 if [ "$IonPot" = "" ];
 then
-   IonPot="XXXXX"
+   IonPot="-----"
 fi
 if [ "$AlphaSOMO" = "" ];
 then
-   AlphaSOMO="XXXXX"
+   AlphaSOMO="-----"
 fi
 if [ "$AlphaLUMO" = "" ];
 then
-   AlphaLUMO="XXXXX"
+   AlphaLUMO="-----"
 fi
 if [ "$BetaSOMO" = "" ];
 then
-   BetaSOMO="XXXXX"
+   BetaSOMO="-----"
 fi
 if [ "$BetaLUMO" = "" ];
 then
-   BetaLUMO="XXXXX"
+   BetaLUMO="-----"
 fi
 if [ "$HOMO" = "" ];
 then
-   HOMO="XXXXX"
+   HOMO="-----"
 fi
 if [ "$LUMO" = "" ];
 then
-   LUMO="XXXXX"
+   LUMO="-----"
 fi
 
 
-echo $filename, $HeatF, $TotalEkcal, $TotalEeV, $DispE, $HBondE, $NHBonds, $IonPot, $AlphaSOMO, $AlphaLUMO, $BetaSOMO, $BetaLUMO, $HOMO, $LUMO >> $1.t379x24
+echo $filename, $HeatF, $TotalEkcal, $TotalEeV, $DispE, $HBondE, $NHBonds, $IonPot, $AlphaSOMO, $AlphaLUMO, $BetaSOMO, $BetaLUMO, $HOMO, $LUMO, $CosmoArea, $CosmoVolume >> $1.t379x24
 done
-
-echo 'Filename', 'Heat of formation (kcal/mol)', 'Total Energy (kcal/mol)', 'Total Energy (eV)', 'Dispersion Energy (eV)', 'H-BOND Ebergy (eV)', 'No. HBonds', 'Ionization Potential (eV)', 'AlphaSOMO (eV)', 'AlphaLUMO (eV)', 'BetaSOMO (eV)', 'BetaLUMO (eV)', 'HOMO (eV)', 'LUMO (eV)' >> t379x24
+echo 'Filename', 'Heat of formation (kcal/mol)', 'Total Energy (kcal/mol)', 'Total Energy (eV)', 'Dispersion Energy (eV)', 'H-BOND Ebergy (eV)', 'No. HBonds', 'Ionization Potential (eV)', 'AlphaSOMO (eV)', 'AlphaLUMO (eV)', 'BetaSOMO (eV)', 'BetaLUMO (eV)', 'HOMO (eV)', 'LUMO (eV)', 'COSMO AREA (square angstroms)', 'COSMO VOLUME (cubic angstroms)' >> t379x24
 cat t379x24 $1.t379x24 > $1.csv
 rm -f t379x24 $1.t379x24
-
